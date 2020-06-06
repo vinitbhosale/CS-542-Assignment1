df9d75f33b8f516b1798d0f132b1854e21830cc2
vbhosal1 <vbhosal1@binghamton.edu>
2020-06-06 19:18:01 -0400

    Metrics handler added.

---
+++ wordPlay/src/wordPlay/handler/MetricsCalculator.java
@@ -0,0 +1,35 @@
+package wordPlay.handler;
+
+import java.util.ArrayList;
+
+import java.io.IOException;
+
+public class MetricsCalculator {
+
+    // Calculate average word length
+  public void calculateMetrics(ArrayList<String> arrLstStr){
+      
+      double counter = 0;
+      double sum = 0;
+      
+      for(String temp: arrLstStr){
+        
+          sum += temp.length();
+          counter++;
+      }
+      double average = sum/counter;
+      System.out.println("AVG_WORD_LENGTH - "+average);
+  }
+  
+  // Calcualte average word per sentance
+  public void calculateMetrics2(ArrayList<String> arrLststr1, double dot){
+  
+      double sum = arrLststr1.size();
+      //System.out.println(sum);
+      double avg1 = sum/dot;
+      System.out.println("AVG_NUM_WORDS_PER_SENTENCE - "+avg1);
+    
+  
+  }
+
+}
